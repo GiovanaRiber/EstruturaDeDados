f6377d6e6c1f4ae5b6809aab849343bdcc2f8f2b
Giovana Ribeiro <giovanaribeir0santos@hotmail.com>
2024-09-30 16:56:04 +0000

    correção

--- trabalho3/eleicao.c
+++ trabalho3/eleicao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "interface.h"
+#include "eleicao.h"
 
 struct chapa { // para armazenar as chapas eleitorais
     char nome[50];
